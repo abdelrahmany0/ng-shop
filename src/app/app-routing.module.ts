e11643d60bf91f7ff570b77348f88b6e1e464d12
mahmoud481 <mah.mohamed48@gmail.com>
2021-04-19 02:36:37 +0200

    show product details & quantity validation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
+import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo:'home', pathMatch:'full'},
+  {path: 'home', component:HomeComponent},
+  {path: 'product/:id', component:ProductDetailsComponent},
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,4 +1,7 @@
+import { ProductsService } from './../services/products.service';
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from '@angular/router'
+import{FormGroup,FormBuilder, Validators, FormControl} from '@angular/forms';
 
 @Component({
   selector: 'app-product-details',
@@ -6,10 +9,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-details.component.css']
 })
 export class ProductDetailsComponent implements OnInit {
+  isLoaded:boolean=false
+  product:any;
+  id:any;
+  quan:number;
+  quantity:FormGroup
+  constructor(private _ProductsService:ProductsService, private _ActivatedRoute:ActivatedRoute, private fb:FormBuilder) { 
+    this.id = this._ActivatedRoute.snapshot.paramMap.get('id');  
+  }
+  
+ 
 
-  constructor() { }
+
+  getFormData(quantity){
+    console.log(quantity);
+  }
+
 
   ngOnInit(): void {
+    this._ProductsService.getProductDetails(this.id).subscribe(data=>{
+      this.product=data.data;
+      console.log(this.product);
+      this.isLoaded=true;
+      this.quan=this.product.Quantity;
+      
+      console.log(this.quan);
+
+      this.quantity=this.fb.group ({
+        'quantity_value' : [1,[Validators.required,Validators.min(1),Validators.max(this.quan)]],
+      });
+    });
+   
+    
   }
+  
 
 }
